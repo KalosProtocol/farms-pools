736b3baa2e761d3a30c1c5b5f768f63208587666
AloneWolf2ksk <35334661+Alonewolf-123@users.noreply.github.com>
2023-01-31 01:55:07 +0700

    added run scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -29,13 +29,13 @@ module.exports = {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: {mnemonic: mnemonic}
+      accounts: [process.env.a2key]
     },
     mainnet: {
       url: "https://bsc-dataseed.binance.org/",
       chainId: 56,
       gasPrice: 20000000000,
-      accounts: {mnemonic: mnemonic}
+      accounts: [process.env.a2key]
     },
     ropsten: {
       url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
